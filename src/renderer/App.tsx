495b5d98af361d318ecf9f70f14a8ffd7f0ce2d3
rocksnow1942 <rocksnow1942@gmail.com>
2022-02-08 17:49:56 -0800

    add

---
+++ src/renderer/App.tsx
@@ -0,0 +1,69 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { shell } from 'electron';
+import Typography from '@mui/material/Typography';
+
+
+const Link = ({children}) => {
+  return <span onClick={()=>{
+    shell.openExternal(children)
+    console.log(children)
+  }}
+  style = {{textDecoration:'underline', cursor:'pointer', color:'blue'}}>
+    {children}
+  </span> 
+}
+
+const Hello = () => {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200px" alt="icon" src={icon} />
+      </div>
+      <h1>electron-react-boilerplate</h1>
+      <div className="Hello">
+        <a
+          href="https://electron-react-boilerplate.js.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Read our docs
+          </button>
+        </a>
+        <Typography variant='subtitle2'>&diams; Paper size definition: <Link>https://pdfkit.org/docs/paper_sizes.html</Link></Typography>
+        <a
+          href="https://github.com/sponsors/electron-react-boilerplate"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              🙏
+            </span>
+            Donate
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
